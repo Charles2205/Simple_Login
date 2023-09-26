d8612a1ac6fe435dacd08fd7fbcd94e761044859
Charles2205 <cewudzie0@gmail.com>
2023-09-26 18:08:09 +0000

    Update : Configured Database connection

--- dbConnect.js
+++ dbConnect.js
@@ -1,1 +1,15 @@
-cons
+const {Sequlize} = reuire('sequelize')
+const dotEnv = require('dotenv').config()
+
+const DB_NAME = process.env.DB_NAME
+const DB_USER = process.env.DB_USER
+const DB_PORT = process.env.DB_PORT
+const DB_PASSWORD = process.env.DB_PASSWORD
+const DB_HOST = process.env.DB_HOST
+
+const sequel = new Sequelize(DB_NAME,DB_USER,DB_PASSWORD,{
+    host:DB_HOST,
+    dialect:'mysql',
+})
+
+module.exports =sequel
